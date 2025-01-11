3c960fb2895cc63ec706d2e582039a7480747fa7
uwugl <137363994+uwu-gl@users.noreply.github.com>
2025-01-12 00:41:23 +0800

    sm6150-common: Add anfangx to build guards

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter courbet davinci phoenix surya sweet toco tucana violet,$(TARGET_DEVICE)),)
+ifneq ($(filter courbet davinci phoenix surya sweet toco tucana violet anfangx,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
